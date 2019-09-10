95c67cd18dcc92a4d0142cd1d2f5009e179758ea
anashlal <50236932+anashlal@users.noreply.github.com>
2019-09-10 10:39:25 +0200

    anasSpring

--- src/main/java/java25/javaspring/App.java
+++ src/main/java/java25/javaspring/App.java
@@ -1,13 +1,10 @@
 package java25.javaspring;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
